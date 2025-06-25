3cdd477a815839ae93265159c2bb538902e8f6c9
Failaliyev <Aliyev.fail@gmail.com>
2025-06-25 13:32:47 +0200

    Update Aufgabe3.java

--- src/Aufgabe3.java
+++ src/Aufgabe3.java
@@ -2,28 +2,28 @@ public class Aufgabe3 {
 
     public static void main(String[] args) {
 
-        // Iki fərqli məhsul üçün hesab yaradılır
+       
 
         Rechnung rechnung1 = new Rechnung(12.5, 3);
 
         Rechnung rechnung2 = new Rechnung(8.0, 5);
 
-        // Ümumi qiymətlər hesablanacaq
+       
         rechnung1.berechneGesamtpreis();
         rechnung2.berechneGesamtpreis();
     }
 }
 
-// Rechnung sinifi – məhsul qiyməti və miqdarı saxlayır
+
 
 
 class Rechnung {
 
-    double preis; // Qiymət (məsələn: 12.5)
-    int anzahl;    // Miqdar (məsələn: 3 ədəd)
+    double preis; 
+    int anzahl;   
 
 
-// Konstruktor – obyekt yaradılarkən qiymət və say verilir
+
 
     public Rechnung(double preis, int anzahl) {
 
@@ -33,7 +33,7 @@ class Rechnung {
 
 
     }
-    // Ümumi qiyməti hesablayan metod
+
 
     public void berechneGesamtpreis() {
 
