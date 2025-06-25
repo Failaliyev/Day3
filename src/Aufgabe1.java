8b0c51c000959620b42fe3f223cc8c3c39b124b7
Failaliyev <Aliyev.fail@gmail.com>
2025-06-25 13:30:14 +0200

    Update Aufgabe1.java

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Aufgabe1 {
     public static void main(String[] args) {
-        Scanner eingabe = new Scanner(System.in); // İstifadəçidən input almaq üçün Scanner
+        Scanner eingabe = new Scanner(System.in); 
 
-        System.out.print("Bitte eine Zahl eingeben: "); // İstifadəçiyə mesaj
-        int zahl = eingabe.nextInt(); // Daxil edilən ədəd int dəyişənə yazılır
+        System.out.print("Bitte eine Zahl eingeben: "); 
+        int zahl = eingabe.nextInt(); 
 
-        // Əgər qalıq 0-dırsa, deməli cütdür
+       
         if (zahl % 2 == 0) {
-            System.out.println("Gerade Zahl."); // Cüt ədəd
+            System.out.println("Gerade Zahl."); 
         } else {
-            System.out.println("Ungerade Zahl."); // Tək ədəd
+            System.out.println("Ungerade Zahl."); 
         }
     }
 }
